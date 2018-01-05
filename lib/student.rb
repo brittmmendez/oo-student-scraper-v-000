1eb5f0d30b9c45b8cb86221d0a8cfd29b6d72e95
Brittany Morris <brittmmendez@gmail.com>
2018-01-05 12:17:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,13 +18,13 @@ class Student
 
   def add_student_attributes(attributes_hash)
     attributes_hash.each do |attribute|
-    @twitter=student_hash[:twitter]
-    @linkedin=student_hash[:linkedin]
-    @github=student_hash[:github]
-    @blog=student_hash[:blog]
-    @profile_quote=student_hash[:profile_quote]
-    @bio=student_hash[:bio]
-    @profile_url=student_hash[:profile_url]
+    @twitter=attributes_hash[:twitter]
+    @linkedin=attributes_hash[:linkedin]
+    @github=attributes_hash[:github]
+    @blog=attributes_hash[:blog]
+    @profile_quote=attributes_hash[:profile_quote]
+    @bio=attributes_hash[:bio]
+    @profile_url=attributes_hash[:profile_url]
   end
   end
 
